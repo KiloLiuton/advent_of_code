6845aa88cce24e1fb3938a09325cbd9bf1fb6599
Kevin Liu Rodrigues <kl@spacetimeanalytics.com>
2025-12-03 10:32:02 -0300

    2025 hooray

---
+++ problems2025/src/p1.rs
@@ -0,0 +1,15 @@
+use std::fs::read_to_string;
+
+pub fn read_lines(filename: &str) -> Vec<String> {
+    let mut result: Vec<String> = Vec::new();
+
+    for line in read_to_string(filename).unwrap().lines() {
+        result.push(line.to_string())
+    }
+
+    result
+}
+
+pub fn foo() {
+    println!("foo");
+}
